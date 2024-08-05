1d1a3ea49ebf6e077c843208b5c8efdb16e1a0dc
LucSosa <l.sosamachado@gmail.com>
2024-08-05 15:59:13 -0300

    Adding radix

--- src/core/components/App.tsx
+++ src/core/components/App.tsx
@@ -1,11 +1,17 @@
+import { Button, Flex, Text } from '@radix-ui/themes';
 import './App.css';
 import { Header } from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+  //  <div className="App">
+  //    <Header></Header>
+  //  </div>
+  <Flex direction="column" gap="2">
+  <Text>Hello from Radix Themes :)</Text>
+  <Button>Let's go</Button>
+  </Flex>
+
   );
 }
 
