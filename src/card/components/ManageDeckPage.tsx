00035c13094743bdd85d45c5b380d88c06397266
LucSosa <l.sosamachado@gmail.com>
2024-08-24 16:36:38 -0300

    Fix: Style cards and Deck

--- src/card/components/ManageDeckPage.tsx
+++ src/card/components/ManageDeckPage.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
-import { Text, Flex, Button, Card } from "@radix-ui/themes";
-import { Page } from "../../core/components/Page";
-import { useGetApi } from "../../core/hooks/useApi";
+import { Button, Card, Flex, Text } from "@radix-ui/themes";
 import { Deck, GCard, GCardEquipament, GCardMonster, GCardSpell } from "../model/gcard";
-import { useAuthData } from "../../player/hooks/useAuthData";
 import { GCardCompactView } from "./GCardCompact";
+import { useAuthData } from "../../player/hooks/useAuthData";
+import { useGetApi } from "../../core/hooks/useApi";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Page } from "../../core/components/Page";
 
 type DeckPreviewProps = {
   monstersCards: GCardMonster[];
@@ -18,62 +18,75 @@ const DeckPreview: React.FC<DeckPreviewProps> = ({
   spellsCards,
   equipamentsCards,
 }) => {
+  const sizeGroup = "250px";
+
   return (
     <>
+      <Text size={"2"}>Monsters</Text>
       <Flex
         direction={"column"}
-        style={{ maxHeight: "300px", overflow: "auto" }}
+        style={{ maxHeight: sizeGroup, overflow: "auto" }}
       >
-        <Text size={"2"}>Monsters</Text>
-        <Flex gap={"2"} direction={'column'} wrap={"nowrap"} style={{background: '#CCCCCC', padding: '15px'}}>
-          {monstersCards.map((card) => (
-            <GCardCompactView gcard={card} />
-          ))}
+        <Flex
+          gap={"2"}
+          direction={"column"}
+          wrap={"nowrap"}
+          style={{ background: "#CCCCCC", padding: "15px" }}
+        >
+          {monstersCards.map((card) => (<GCardCompactView gcard={card} />))}
         </Flex>
+    </Flex>
+
+    <Text size={"2"}>Spells</Text>
+    <Flex
+      direction={"column"}
+      gap={"2"}
+      style={{ maxHeight: sizeGroup, overflow: "auto" }}
+    >
+      <Flex
+        gap={"2"}
+        direction={"column"}
+        wrap={"nowrap"}
+        style={{ background: "#CCCCCC", padding: "15px" }}
+      >
+        {spellsCards.map((card) => (<GCardCompactView gcard={card} />))}
       </Flex>
-      <Flex direction={"column"} gap={"2"}>
-        <Text size={"2"}>Spells</Text>
-        {spellsCards.map((card) => (
-          <GCardCompactView gcard={card} />
-        ))}
+    </Flex>
+      
+    <Text size={"2"}>Equipaments</Text>
+    <Flex
+      direction={"column"}
+      gap={"2"}
+      style={{ maxHeight: sizeGroup, overflow: "auto" }}
+    >
+      <Flex
+        gap={"2"}
+        direction={"column"}
+        wrap={"nowrap"}
+        style={{ background: "#CCCCCC", padding: "15px" }}
+      >
+        {equipamentsCards.map((card) => (<GCardCompactView gcard={card} />))}
       </Flex>
-      <Flex direction={"column"} gap={"2"}>
-        <Text size={"2"}>Equipaments</Text>
-        {equipamentsCards.map((card) => (
-          <GCardCompactView gcard={card} />
-        ))}
-      </Flex>
-    </>
+    </Flex>
+  </>
   );
 };
 
 export const ManageDeckPage = () => {
   const userId = useAuthData();
   const deckResponse = useGetApi<Deck[]>(`http://localhost:5000/user/${userId}/deck`);
-  const cardResponse = useGetApi<GCard[]>("http://localhost:5500/gcard");
-
-  const [deckMonstersCards, setDeckMonstersCards] = useState<GCardMonster[]>(
-    []
-  );
+  const cardResponse = useGetApi<GCard[]>("http://localhost:5000/gcard");
+  const [deckMonstersCards, setDeckMonstersCards] = useState<GCardMonster[]>([]);
   const [deckSpellsCards, setDeckSpellsCards] = useState<GCardSpell[]>([]);
-  const [deckEquipamentsCards, setDeckEquipamentsCards] = useState<
-    GCardEquipament[]
-  >([]);
-
-  const [userMonstersCards, setUserMonstersCards] = useState<GCardMonster[]>(
-    []
-  );
+  const [deckEquipamentsCards, setDeckEquipamentsCards] = useState<GCardEquipament[]>([]);
+  const [userMonstersCards, setUserMonstersCards] = useState<GCardMonster[]>([]);
   const [userSpellsCards, setUserSpellsCards] = useState<GCardSpell[]>([]);
-  const [userEquipamentsCards, setUserEquipamentsCards] = useState<
-    GCardEquipament[]
-  >([]);
-
+  const [userEquipamentsCards, setUserEquipamentsCards] = useState<GCardEquipament[]>([]);
   useEffect(() => {
     if (cardResponse && cardResponse.data) {
       const monsterCards: GCardMonster[] = [];
       const spellCards: GCardSpell[] = [];
       const equipamentCards: GCardEquipament[] = [];
-
       cardResponse.data.forEach((card) => {
         if (card.type === "monster") {
           monsterCards.push(card as GCardMonster);
@@ -85,7 +98,6 @@ export const ManageDeckPage = () => {
           equipamentCards.push(card as GCardEquipament);
         }
       });
-
       setUserMonstersCards(monsterCards);
       setUserSpellsCards(spellCards);
       setUserEquipamentsCards(equipamentCards);
@@ -95,25 +107,26 @@ export const ManageDeckPage = () => {
   return (
     <Page>
       <Flex justify={"center"} align={"center"} direction={"column"}>
-        <Link to={'/user'} style={{alignSelf: 'end', margin: '10px'}}><Button>Back</Button></Link>
-        <Text size={"5"}>Manage deck</Text>
-
-        <Card>
-            <Flex justify={'between'} minWidth={'300px'}>
-                <Flex direction={'column'} justify={'center'} align={'center'}>
-                    <strong>Monsters</strong>
-                    20 / 20
-                </Flex>
-                <Flex direction={'column'} justify={'center'} align={'center'}>
-                    <strong>Spells</strong>
-                    10 / 10
-                </Flex>
-                <Flex direction={'column'} justify={'center'} align={'center'}>
-                    <strong>Equipaments</strong>
-                    5 / 5
-                </Flex>
+        <Flex justify={"between"} align={"center"} width={"100%"}>
+          <Card style={{margin: '10px'}}>
+            <Flex justify={"between"} minWidth={"300px"}>
+              <Flex direction={"column"} justify={"center"} align={"center"}>
+                <strong>Monsters</strong>
+                20 / 20
+              </Flex>
+              <Flex direction={"column"} justify={"center"} align={"center"}>
+                <strong>Spells</strong>
+                10 / 10
+              </Flex>
+              <Flex direction={"column"} justify={"center"} align={"center"}>
+                <strong>Equipaments</strong>5 / 5
+              </Flex>
             </Flex>
-        </Card>
+          </Card>
+          <Link to={"/user"} style={{ alignSelf: "end", margin: "10px" }}>
+            <Button>Back</Button>
+          </Link>
+        </Flex>
 
         <Flex
           style={{ width: "100%", marginTop: "15px" }}
@@ -128,7 +141,6 @@ export const ManageDeckPage = () => {
               </Text>
               <Text size={"2"}>min cards: 25</Text>
             </Flex>
-
             <DeckPreview
               monstersCards={deckMonstersCards}
               spellsCards={deckSpellsCards}
@@ -145,7 +157,6 @@ export const ManageDeckPage = () => {
                 Cards
               </Text>
             </Flex>
-
             <DeckPreview
               monstersCards={userMonstersCards}
               spellsCards={userSpellsCards}
